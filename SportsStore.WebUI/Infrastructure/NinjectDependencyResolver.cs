08adb34c5a3de119d17b8479828a4101692599ae
Ahmed Ali <aa_sen1980@yahoo.com>
2019-05-19 04:46:30 +0300

    until 2015

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,4 +1,7 @@
-﻿using Ninject;
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +29,13 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             // put bindings here
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                 new Product { Name = "Football", Price = 25 },
+                      new Product { Name = "Surf board", Price = 179 },
+                     new Product { Name = "Running shoes", Price = 95 }});
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
